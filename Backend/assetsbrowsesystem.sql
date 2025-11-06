39abeda7a0895ac85a86fecb88146449da051650
RapinPromsen <6631501098@lamduan.mfu.ac.th>
2025-11-06 11:35:30 +0700

    Add a little bit borrow_requests

--- Backend/assetsbrowsesystem.sql
+++ Backend/assetsbrowsesystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 30, 2025 at 09:21 PM
+-- Generation Time: Nov 06, 2025 at 04:34 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.26
 
@@ -42,10 +42,10 @@ CREATE TABLE `assets` (
 --
 
 INSERT INTO `assets` (`id`, `code`, `name`, `description`, `status`, `image_url`, `created_at`) VALUES
-(2, 'AS-002', 'Camera', NULL, 'pending', '/uploads/1761809104954.png', '2025-10-30 07:25:04'),
-(3, 'AS-003', 'Camera', NULL, 'borrowed', '/uploads/1761809105815.png', '2025-10-30 07:25:05'),
-(4, 'AS-004', 'Camera', NULL, 'disabled', '/uploads/1761809106627.png', '2025-10-30 07:25:06'),
-(5, 'AS-005', 'Camera', NULL, 'available', '/uploads/1761809107425.png', '2025-10-30 07:25:07');
+(2, 'AS-002', 'Camera', NULL, 'disabled', '/uploads/1761809104954.png', '2025-10-30 07:25:04'),
+(3, 'AS-003', 'Camera', NULL, 'available', '/uploads/1761809105815.png', '2025-10-30 07:25:05'),
+(4, 'AS-004', 'Camera', NULL, 'available', '/uploads/1761809106627.png', '2025-10-30 07:25:06'),
+(5, 'AS-005', 'Router', 'TEST', 'available', '/uploads/1762105726451.png', '2025-10-30 07:25:07');
 
 -- --------------------------------------------------------
 
@@ -61,9 +61,18 @@ CREATE TABLE `borrow_requests` (
   `return_date` date NOT NULL,
   `status` enum('pending','approved','rejected','returned') DEFAULT 'pending',
   `decided_by` int DEFAULT NULL,
+  `got_back_by` int DEFAULT NULL,
   `decided_at` datetime DEFAULT NULL,
+  `decision_note` text,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `borrow_requests`
+--
+
+INSERT INTO `borrow_requests` (`id`, `requester_id`, `asset_id`, `borrow_date`, `return_date`, `status`, `decided_by`, `got_back_by`, `decided_at`, `decision_note`, `created_at`) VALUES
+(4, 3, 3, '2025-11-06', '2025-11-06', 'returned', 2, 1, '2025-11-06 03:23:36', 'Very Good', '2025-11-05 20:21:52');
 
 -- --------------------------------------------------------
 
@@ -80,6 +89,13 @@ CREATE TABLE `request_history` (
   `changed_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `note` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `request_history`
+--
+
+INSERT INTO `request_history` (`id`, `request_id`, `old_status`, `new_status`, `changed_by_id`, `changed_at`, `note`) VALUES
+(2, 4, 'approved', 'returned', 1, '2025-11-05 20:23:36', 'Returned by staff');
 
 -- --------------------------------------------------------
 
@@ -103,7 +119,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `full_name`, `username`, `password`, `role`, `created_at`) VALUES
 (1, 'Staff One', 'staff1', '$2b$10$j0CVcBRiwROpT.iKiZ.mRu9sjTSaG/co1zGjx1ZPxBC5Z3SYgxoYi', 'STAFF', '2025-10-30 06:50:03'),
 (2, 'Lecture One', 'lecture1', '$2b$10$3cqgFSAj5a6jH6JQLjcT6uQ/zxKBhkv5Oop8Us2F.KiaVYsPFIvei', 'LECTURER', '2025-10-30 07:23:25'),
-(3, 'Student One', 'student1', '$2b$10$rnvjbIPhm0toUKEkPF0IaOoPfp2pWX1/E.733/gaN4Yl14DdiYS/S', 'STUDENT', '2025-10-30 07:24:12');
+(3, 'Student One', 'student1', '$2b$10$rnvjbIPhm0toUKEkPF0IaOoPfp2pWX1/E.733/gaN4Yl14DdiYS/S', 'STUDENT', '2025-10-30 07:24:12'),
+(4, 'test', 'test123', '$2b$10$oziqJ99L3O6F92fPx2h3seM3sHk/oPIhcwvSGg4cDHStxYSDFEBM2', 'STUDENT', '2025-10-31 02:06:35'),
+(5, 'test123123', 'test2222', '$2b$10$6dnUZaC8gPQyk423TzIrieCevyMhcDQyX3RfD.W6/lITY9jqCxYqC', 'STUDENT', '2025-10-31 03:19:38'),
+(6, 'test1231', 'test2', '$2b$10$0YWZiykrWEBk24qRMUkQPussjIt/FSGnb8bajMgYAfSHIiHRj.myy', 'STUDENT', '2025-10-31 03:43:49');
 
 --
 -- Indexes for dumped tables
@@ -154,19 +173,19 @@ ALTER TABLE `assets`
 -- AUTO_INCREMENT for table `borrow_requests`
 --
 ALTER TABLE `borrow_requests`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `request_history`
 --
 ALTER TABLE `request_history`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
